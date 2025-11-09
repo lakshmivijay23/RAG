8a9d64e44c24cabc022c0e9ee3d4ade91937e582
Lakshmi Vijayan <146443780+lakshmivijay23@users.noreply.github.com>
2025-11-09 18:21:49 +0530

    Delete traditional_rag.py

--- traditional_rag.py
+++
@@ -1,9 +0,0 @@
-"""
-traditional rag follows 'retrive and generate' paradigm - just grabs info from a set sources and answers based on that
-
-- retreives semantically similar but contextually irrelevant info
-- misses multi-hop reasoning and deep contextual understanding
-- scalability issues as knowledge base grows: context window limits and high computational overhead
-- lacks dynamic retrieval: cannot adapt to new info or changing user intents or feedback or evolving contexts
-
-"""
